e5a54d17f2063f1a0273925c0a78d32f241a1371
carreuky <carreuky@gmail.com>
2022-07-26 18:09:45 +0300

    buttons and search input

--- src/Components/Jobs/Jobs.js
+++ src/Components/Jobs/Jobs.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from "react";
+import "./Jobs.css";
+
 export default function Jobs() {
-  return (
-    <div>Jobs
-<i class="fa-brands fa-facebook"></i>
+  const Search = (
+    <div class="container m-3 ">
+    <div class="d-flex justify-content-center px-2 ">
+    <div class="form-group">
+    <input class="form-control input-lg" id="inputlg"  placeholder="Search..."/>
+  </div>
     </div>
-  )
+  </div>
+  );
+
+  return (
+    <div className="Jobs p-2">
+      <h1 class="text-center bg-primary">Search Jobs in Kenya</h1>
+      <div className="text-center">
+        <button type="button" class="btn btn-primary btn-sm mx-2">
+          Jobs in Kenya
+        </button>
+        <button type="button" class="btn btn-primary btn-sm mx-2">
+          Jobs in Africa
+        </button>
+        <button type="button" class="btn btn-primary btn-sm mx-2">
+          Jobs Abroad
+        </button>
+      </div>
+      {Search}
+    </div>
+  );
 }
